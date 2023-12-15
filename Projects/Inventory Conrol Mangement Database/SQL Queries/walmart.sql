1894d6bbc92eb7b3428e37adf29a89173b0471d8
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-12-15 17:57:17 +0300

    final commit

--- Projects/Inventory Conrol Mangement Database/SQL Queries/walmart.sql
+++ Projects/Inventory Conrol Mangement Database/SQL Queries/walmart.sql
@@ -4,3 +4,15 @@ SELECT
      MAX([Weekly_Sales]) AS MAXIMUM_WEEKLY_SALES
   FROM [Walmart Sales].[dbo].[Walmart$]
 
+
+/****** How was the weather during the year of highest sales?  ******/
+SELECT TOP (1000) [Store]
+      ,[Date]
+      ,[Weekly_Sales]
+      ,[Holiday_Flag]
+      ,[Temperature]
+      ,[Fuel_Price]
+      ,[CPI]
+      ,[Unemployment]
+  FROM [Walmart Sales].[dbo].[Walmart$]
+  ORDER BY Weekly_Sales;
