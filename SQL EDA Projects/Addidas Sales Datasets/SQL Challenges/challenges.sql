3e041511e12e6248b3428994f48671dd1dc7ebe0
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-04 14:17:47 +0300

    final

--- SQL EDA Projects/Addidas Sales Datasets/SQL Challenges/challenges.sql
+++ SQL EDA Projects/Addidas Sales Datasets/SQL Challenges/challenges.sql
@@ -26,10 +26,10 @@ SELECT Product,COUNT(DISTINCT [Retailer ID]) AS_Retail_Count
   FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
   GROUP BY Product
   ORDER BY AS_Retail_Count DESC;
-
-
-
-
-
-
 -- which  Sales method  had the most retailers ---
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT [Sales Method],COUNT(DISTINCT [Retailer ID]) As_Retailer_Count
+  FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
+  GROUP BY [Sales Method]
+  Order BY As_Retailer_Count DESC;
+
