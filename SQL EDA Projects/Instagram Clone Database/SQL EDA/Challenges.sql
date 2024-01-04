2b8afef495961696318b9de60dab27261c52bca2
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-04 09:48:23 +0300

    final

--- SQL EDA Projects/Instagram Clone Database/SQL EDA/Challenges.sql
+++ SQL EDA Projects/Instagram Clone Database/SQL EDA/Challenges.sql
@@ -2,3 +2,14 @@
 SELECT * FROM ig_clone.users
 ORDER BY created_at DESC
 LIMIT 10;
+
+-- whick day of the week do most users regietred on ?
+-- We need to schedule out when  to schedule an ad campaign -- 
+
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
