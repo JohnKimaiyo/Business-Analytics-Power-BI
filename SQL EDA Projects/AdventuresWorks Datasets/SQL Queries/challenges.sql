a2ba8e07e937d2e40731803398e304780615061c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-04 17:46:56 +0300

    final

--- SQL EDA Projects/AdventuresWorks Datasets/SQL Queries/challenges.sql
+++ SQL EDA Projects/AdventuresWorks Datasets/SQL Queries/challenges.sql
@@ -7,8 +7,15 @@ SELECT TOP 5
    ORDER BY BirthDate ASC
 
 -- Lets reward the top ten oldest customers based on the  order dates --
-/****** Script for SelectTopNRows command from SSMS  ******/
+
 SELECT TOP 10 *
      
   FROM [AdventureWorksDW2022].[dbo].[DimCustomer]
   ORDER BY [DateFirstPurchase] ASC;
+  -- Which titles has the most emloyees ---
+  
+SELECT Title,COUNT(Distinct[EmployeeNationalIDAlternateKey]) As_Emplyee_count
+    
+  FROM [AdventureWorksDW2022].[dbo].[DimEmployee]
+  GROUP BY Title
+  Order By As_Emplyee_count DESC;
