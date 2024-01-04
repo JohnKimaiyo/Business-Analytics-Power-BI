a16c3b6c05f47b84a5da6c98bcaf16cbbe919a46
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-04 14:06:04 +0300

    final commit

--- SQL EDA Projects/Addidas Sales Datasets/SQL Challenges/challenges.sql
+++ SQL EDA Projects/Addidas Sales Datasets/SQL Challenges/challenges.sql
@@ -10,7 +10,13 @@ SELECT Region ,COUNT(DISTINCT [Retailer ID]) As_Retailer_Count
 
 
 -- which  State  had the most retailers ---
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT State,COUNT([Retailer ID]) AS Retailler_Count
+ 
+  FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
+  GROUP BY State
 
+  ORDER BY Retailler_Count Desc;
 
 
 
