442a350a7279c28ce6350b5646ddb0dd85c91748
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-04 17:37:54 +0300

    final

--- SQL EDA Projects/AdventuresWorks Datasets/SQL Queries/challenges.sql
+++ SQL EDA Projects/AdventuresWorks Datasets/SQL Queries/challenges.sql
@@ -1,8 +1,14 @@
 -- Get the Top Ten Oldest Customers --
-/****** Script for SelectTopNRows command from SSMS  ******/
+
 SELECT TOP 5
  
   *
   FROM [AdventureWorksDW2022].[dbo].[DimCustomer]
-   ORDER BY BirthDate DESC
-   
+   ORDER BY BirthDate ASC
+
+-- Lets reward the top ten oldest customers based on the  order dates --
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP 10 *
+     
+  FROM [AdventureWorksDW2022].[dbo].[DimCustomer]
+  ORDER BY [DateFirstPurchase] ASC;
