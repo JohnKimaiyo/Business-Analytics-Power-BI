cab58e7bed39efe0dc1cfdb4f5185f00e7e3ae82
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-14 21:09:45 +0300

    final

--- Projects/Python/Electric Vehicles/SQL/SQL EDA.sql
+++ Projects/Python/Electric Vehicles/SQL/SQL EDA.sql
@@ -1,4 +1,8 @@
 -- write SQL to get the average electric range -- 
-/****** Script for SelectTopNRows command from SSMS  ******/
 SELECT AVG([Electric Range]) AS Average_Electric_Range
+  FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
+
+  
+-- write SQL to get the Maximumelectric range --
+SELECT MAX([Electric Range]) AS  Maximum_Electric_Range
   FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
