c78f8dfc642a48cf8ed02402c71fb3ab223c00f8
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-14 21:29:51 +0300

    final

--- Projects/Python/Electric Vehicles/SQL/SQL EDA.sql
+++ Projects/Python/Electric Vehicles/SQL/SQL EDA.sql
@@ -6,3 +6,10 @@ SELECT AVG([Electric Range]) AS Average_Electric_Range
 -- write SQL to get the Maximumelectric range --
 SELECT MAX([Electric Range]) AS  Maximum_Electric_Range
   FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
+
+
+--  Provide top ten counties that  had the hightest Electric range suing SQL ---
+SELECT TOP 10 County, SUM([Electric Range]) AS TotalElectricRange
+FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
+GROUP BY County
+ORDER BY TotalElectricRange DESC
