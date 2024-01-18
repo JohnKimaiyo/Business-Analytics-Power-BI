2346a0bc52a102849aceb4d97fb8ecd33d43ec63
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-18 11:51:08 +0300

    final

--- Projects/Python/Real Estate Sales 2001-2020 GL/SQL/real_estate.sql
+++ Projects/Python/Real Estate Sales 2001-2020 GL/SQL/real_estate.sql
@@ -1,3 +1,7 @@
--- Dispplay the highest vale accross the years --
+-- Display the highest assessed of the properties value accross the years --
 SELECT MAX([Assessed Value]) AS Highest_Assesed_Values_Accross_All_Years
   FROM [Real Estate Sales Dataset].[dbo].['Real_Estate_Sales_2001-2020_GL$']
+
+-- Display the average  assesed value of the properties  across the years --
+SELECT ROUND(AVG([Assessed Value]), 0) AS Average_Assessed_Values_Across_All_Years
+FROM [Real Estate Sales Dataset].[dbo].['Real_Estate_Sales_2001-2020_GL$']
