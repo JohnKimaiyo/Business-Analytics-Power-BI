98759a59cbd7345f7decdd7a7a193cbf1f16b54f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-18 13:39:13 +0300

    final

--- Projects/Python/U.S. Electric Utility Companies and Rates/SQL/US Electricity rates.sql
+++ Projects/Python/U.S. Electric Utility Companies and Rates/SQL/US Electricity rates.sql
@@ -1,3 +1,8 @@
 /****** What is the maximum commercial rate  ******/
 SELECT MAX([comm_rate]) AS Highest_Commercial_Rate
   FROM [U.S. Electric Utility Companies and Rates ].[dbo].['US Electricity Rates$']
+
+
+  /****** What is the Average Commercial Rates  ******/
+SELECT AVG([comm_rate]) AS Average_Commercial_rate
+  FROM [U.S. Electric Utility Companies and Rates ].[dbo].['US Electricity Rates$']
