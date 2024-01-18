4b815dc4f7264a8b09ef06bd0b753e15649ccd04
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-18 17:49:17 +0300

    final

--- Projects/Python/Allegheny County Employee Salaries/SQL/alleghey employee.sql
+++ Projects/Python/Allegheny County Employee Salaries/SQL/alleghey employee.sql
@@ -12,3 +12,10 @@ SELECT TOP 10[DEPARTMENT],SUM([ GROSS_PAY ]) AS Total_Salary_Paid
 FROM [Allegheny County Employee Salaries].[dbo].['Allegheny  county salaries$']
 GROUP BY [DEPARTMENT]
 ORDER BY Total_Salary_Paid DESC;
+
+
+/******Which top ten Job Titles had the highest salaries  ******/
+SELECT TOP 10 [JOB_TITLE] , SUM([ GROSS_PAY ])  AS Total_Salary_Paid
+FROM [Allegheny County Employee Salaries].[dbo].['Allegheny  county salaries$']
+GROUP BY [JOB_TITLE]
+ORDER BY Total_Salary_Paid DESC;
