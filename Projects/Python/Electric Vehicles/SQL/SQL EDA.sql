e5a5a04ff117050ed18e6eb0b08e4b4de75ac356
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-18 10:33:12 +0300

    final

--- Projects/Python/Electric Vehicles/SQL/SQL EDA.sql
+++ Projects/Python/Electric Vehicles/SQL/SQL EDA.sql
@@ -19,11 +19,51 @@ ORDER BY TotalElectricRange DESC
 SELECT TOP  10 City,  SUM([Electric Range]) AS Total_Electric_Range
 FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
 GROUP BY City
-ORDER BY Total_Electric_Range
+ORDER BY Total_Electric_Range DESC;
 
 
 --  Provide top ten Car Make that  had the hightest Electric range suing SQL ---
 SELECT TOP 10 Make , SUM([Electric Range] ) AS Total_Electric_Range
 FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
 GROUP BY Make
-ORDER BY Total_Electric_Range
+ORDER BY Total_Electric_Range DESC;
+
+--  Provide top ten CaR Model Make that  had the hightest Electric range suing SQL ---
+SELECT TOP 10 Model, SUM([Electric Range] ) AS Total_Electric_Range
+FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
+GROUP BY Model
+ORDER BY Total_Electric_Range DESC;
+
+
+--  Provide top ten CaR Clean Alternative Fuel Vehicle (CAFV) Eligibility]   that  had the hightest Electric range suing SQL ---
+SELECT TOP 10
+    [Clean Alternative Fuel Vehicle (CAFV) Eligibility],
+    SUM([Electric Range]) AS Total_Electric_Range
+FROM
+    [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
+GROUP BY
+    [Clean Alternative Fuel Vehicle (CAFV) Eligibility]
+ORDER BY
+    Total_Electric_Range DESC;
+
+
+--  Provide top ten Electric Vehicle Type  that  had the hightest Electric range suing SQL ---
+
+SELECT TOP 10  [Electric Vehicle Type] , SUM ([Electric Range]) AS Total_Electric_Range
+     
+  FROM [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
+  GROUP BY [Electric Vehicle Type]
+  ORDER BY Total_Electric_Range DESC;
+
+-- Display top ten Years that had the highest Electric Range
+SELECT TOP 10 
+    [Model Year],
+    SUM([Electric Range]) AS TotalElectricRange
+ 
+FROM
+    [Electric Vehicle Population].[dbo].[Electric_Vehicle_Population_Dat$]
+GROUP BY
+    [Model Year]
+ORDER BY
+    TotalElectricRange DESC;
+
