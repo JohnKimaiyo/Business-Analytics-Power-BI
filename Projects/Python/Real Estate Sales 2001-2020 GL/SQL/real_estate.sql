6cf7cd4ee2e4f5fd7bdd06edf4d879245b367d03
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-18 12:19:34 +0300

    final

--- Projects/Python/Real Estate Sales 2001-2020 GL/SQL/real_estate.sql
+++ Projects/Python/Real Estate Sales 2001-2020 GL/SQL/real_estate.sql
@@ -24,3 +24,10 @@ SELECT TOP 10 [Town] ,SUM([Sale Amount]) AS Total_Assessed_Value
   FROM [Real Estate Sales Dataset].[dbo].['Real_Estate_Sales_2001-2020_GL$']
   GROUP BY [Town]
   ORDER BY Total_Assessed_Value DESC;
+
+
+/****** What is the top ten Property types based on Property sales across the years ******/
+SELECT TOP 10 [Property Type], SUM([Sale Amount]) AS Total_Property_Sales
+  FROM [Real Estate Sales Dataset].[dbo].['Real_Estate_Sales_2001-2020_GL$']
+  GROUP BY [Property Type]
+  ORDER BY Total_Property_Sales DESC;
