cd9bd72394c4b0db345925c9816323377bd1d925
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-18 17:37:27 +0300

    final

--- Projects/Python/Allegheny County Employee Salaries/SQL/alleghey employee.sql
+++ Projects/Python/Allegheny County Employee Salaries/SQL/alleghey employee.sql
@@ -1,3 +1,7 @@
 /****** What is the highest salary paid  ******/
 SELECT MAX([ GROSS_PAY ]) AS Highest_Salary
   FROM [Allegheny County Employee Salaries].[dbo].['Allegheny  county salaries$']
+
+  /****** What is the Average salary paid  ******/
+SELECT AVG([ GROSS_PAY ]) AS Average_Salary
+  FROM [Allegheny County Employee Salaries].[dbo].['Allegheny  county salaries$']
