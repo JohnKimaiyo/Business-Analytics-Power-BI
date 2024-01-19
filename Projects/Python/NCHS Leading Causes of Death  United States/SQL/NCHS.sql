26f5673cb7439bda4fae46d9a72f9fd674ef1b3d
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 12:42:46 +0300

    final

--- Projects/Python/NCHS Leading Causes of Death  United States/SQL/NCHS.sql
+++ Projects/Python/NCHS Leading Causes of Death  United States/SQL/NCHS.sql
@@ -1,4 +1,8 @@
 /****** What is the total Number of Deaths  ******/
 SELECT MAX([Deaths]) AS Total_Number_of_Deaths
 FROM [NCHS - Leading Causes of Death: United States].[dbo].['NCHS_-_Leading_Causes_of_Death_$']
+
+/****** What is the Average Number of Deaths ******/
+SELECT AVG([Deaths]) AS  Average_Number_of_Deaths
+FROM [NCHS - Leading Causes of Death: United States].[dbo].['NCHS_-_Leading_Causes_of_Death_$']
   
