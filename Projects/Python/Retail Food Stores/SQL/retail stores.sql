cc16927d89eaf01e2dc318967d2b735f58869724
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 15:18:01 +0300

    final

--- Projects/Python/Retail Food Stores/SQL/retail stores.sql
+++ Projects/Python/Retail Food Stores/SQL/retail stores.sql
@@ -6,3 +6,10 @@ FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
 /****** What is the Average square feet  ******/
 SELECT AVG([Square Footage]) AS Average_Square_Feet
 FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+
+/****** What are the top ten Counties with the highest square feet ******/
+SELECT TOP 10  [County] ,  SUM([Square Footage]) AS Total_Square_Feet
+      
+  FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+  GROUP BY [County]
+  ORDER BY  Total_Square_Feet DESC;
