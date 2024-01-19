f8274ff60ec65d1c24a7fa9fe7de3317b64daf0a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 10:17:27 +0300

    final

--- Projects/SQL/SQL EDA Projects/Addidas Sales Datasets/SQL Challenges/challenges.sql
+++ Projects/SQL/SQL EDA Projects/Addidas Sales Datasets/SQL Challenges/challenges.sql
@@ -2,34 +2,7 @@
 SELECT MAX([Total Sales]) Highest_Sales_Amount
   FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
 
--- which  State  had the most retailers ---
 
-SELECT State,COUNT([Retailer ID]) AS Retailler_Count
- 
+/****** Whats is the average sales amount  ******/
+SELECT AVG([Total Sales]) Average_Sales_Amount
   FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
-  GROUP BY State
-
-  ORDER BY Retailler_Count Desc;
-
-
--- which  Products had the most retailers ---
-
-
-SELECT Product,COUNT(DISTINCT [Retailer ID]) AS_Retail_Count
-  FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
-  GROUP BY Product
-  ORDER BY AS_Retail_Count DESC;
--- which  Sales method  had the most retailers ---
-
-SELECT [Sales Method],COUNT(DISTINCT [Retailer ID]) As_Retailer_Count
-  FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
-  GROUP BY [Sales Method]
-  Order BY As_Retailer_Count DESC;
-
--- which Retailers  had the most purchases
-
-SELECT Retailer , COUNT(DISTINCT[Retailer ID]) AS_Retailer_Count
-     
-  FROM [Addidas Sales Datasets].[dbo].['Data Sales Adidas$']
-  GROUP BY Retailer
-  ORDER BY AS_Retailer_Count DESC;
