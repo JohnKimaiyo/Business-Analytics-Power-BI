22511202c41f8aa0fdb6746d9c42fb49498b0895
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 11:29:00 +0300

    final

--- Projects/SQL/SQL EDA Projects/Air Traffic Passenger Statistics/SQL Queries/Sanfrasisco.sql
+++ Projects/SQL/SQL EDA Projects/Air Traffic Passenger Statistics/SQL Queries/Sanfrasisco.sql
@@ -1,3 +1,8 @@
 /****** Whats is the highest the Passenger Count  ******/
 SELECT MAX([Passenger Count]) AS Highest_Passenger_Count
 FROM [San Francisco International Airport Air Traffic].[dbo].[Air_Traffic_Passenger_Statistic$]
+
+/******  Whats is the Average the Passenger Count   ******/
+SELECT AVG([Passenger Count]) AS Average_Passenger_Count
+FROM [San Francisco International Airport Air Traffic].[dbo].[Air_Traffic_Passenger_Statistic$]
+
