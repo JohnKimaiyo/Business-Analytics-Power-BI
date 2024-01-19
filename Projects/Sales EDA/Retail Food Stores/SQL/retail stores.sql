6d852d1ef7c78ef4c299815717536a3dc6681999
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 17:26:50 +0300

    final

---
+++ Projects/Sales EDA/Retail Food Stores/SQL/retail stores.sql
@@ -0,0 +1,27 @@
+/****** What is the Maximum square feet  ******/
+SELECT MAX([Square Footage]) AS Highest_Square_Feet
+FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+
+
+/****** What is the Average square feet  ******/
+SELECT AVG([Square Footage]) AS Average_Square_Feet
+FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+
+/****** What are the top ten Counties with the highest square feet ******/
+SELECT TOP 10  [County] ,  SUM([Square Footage]) AS Total_Square_Feet
+      
+  FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+  GROUP BY [County]
+  ORDER BY  Total_Square_Feet DESC;
+
+  /******  What are the top ten Establishment Type with the highest square feet  ******/
+SELECT TOP 10 [Establishment Type], SUM([Square Footage]) AS Total_Square_Feet
+  FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+  GROUP BY  [Establishment Type]
+  ORDER BY Total_Square_Feet DESC;
+
+  /******  What are the top ten Cities  with the highest square feet  ******/
+SELECT TOP 10 [City]  , SUM([Square Footage]) AS Total_Square_Feet
+FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+GROUP BY [City] 
+ORDER BY Total_Square_Feet DESC;
