41d11c06579b55ad57799a4015252b789e824bba
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 14:21:10 +0300

    final

--- Projects/Python/NCHS Leading Causes of Death  United States/SQL/NCHS.sql
+++ Projects/Python/NCHS Leading Causes of Death  United States/SQL/NCHS.sql
@@ -12,3 +12,9 @@ SELECT TOP 10 [Cause Name] ,  SUM([Deaths]) AS Total_Sum_Of_Death
 FROM [NCHS - Leading Causes of Death: United States].[dbo].['NCHS_-_Leading_Causes_of_Death_$']
 GROUP BY [Cause Name] 
 ORDER BY Total_Sum_Of_Death DESC;
+
+/****** Whats is the top ten States with the highest deaths   ******/
+SELECT TOP 10 [State] , SUM([Deaths]) AS Total_Deaths
+  FROM [NCHS - Leading Causes of Death: United States].[dbo].['NCHS_-_Leading_Causes_of_Death_$']
+  GROUP BY [State]
+  ORDER BY Total_Deaths DESC;
