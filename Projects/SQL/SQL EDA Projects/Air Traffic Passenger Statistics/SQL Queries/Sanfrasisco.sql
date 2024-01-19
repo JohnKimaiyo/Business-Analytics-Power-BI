ceeb0bb61be0b6a2341436e3c0c5c5784d362f61
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 12:01:35 +0300

    final

--- Projects/SQL/SQL EDA Projects/Air Traffic Passenger Statistics/SQL Queries/Sanfrasisco.sql
+++ Projects/SQL/SQL EDA Projects/Air Traffic Passenger Statistics/SQL Queries/Sanfrasisco.sql
@@ -18,3 +18,10 @@ SELECT TOP 2 [GEO Summary],  SUM([Passenger Count]) AS Sum_Of_Passenger_Count
 FROM [San Francisco International Airport Air Traffic].[dbo].[Air_Traffic_Passenger_Statistic$]
 GROUP BY [GEO Summary]
 ORDER BY Sum_Of_Passenger_Count DESC;
+
+
+-- Display the top destinationsbased on passegnger numbers --
+SELECT TOP 10 [GEO Region], SUM([Passenger Count]) AS Sum_of_Passanger_Numbers
+FROM [San Francisco International Airport Air Traffic].[dbo].[Air_Traffic_Passenger_Statistic$]
+GROUP BY [GEO Region]
+ORDER BY  Sum_of_Passanger_Numbers DESC;
