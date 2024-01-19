c09fd760540c19f7b93fc0873545745c6c79d77c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 15:14:56 +0300

    final

--- Projects/Python/Retail Food Stores/SQL/retail stores.sql
+++ Projects/Python/Retail Food Stores/SQL/retail stores.sql
@@ -1,3 +1,8 @@
 /****** What is the maximum square feet  ******/
 SELECT MAX([Square Footage]) AS Highest_Square_Feet
 FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
+
+
+/****** What is the Average square feet  ******/
+SELECT AVG([Square Footage]) AS Average_Square_Feet
+FROM [State of NewYork Retail Food Stores].[dbo].[Retail_Food_Stores$]
