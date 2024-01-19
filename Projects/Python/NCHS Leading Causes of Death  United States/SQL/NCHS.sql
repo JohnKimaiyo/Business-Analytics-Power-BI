a76d039e2a49ec487d9a03ed8c1216b97c70d4a6
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-19 12:39:03 +0300

    final

---
+++ Projects/Python/NCHS Leading Causes of Death  United States/SQL/NCHS.sql
@@ -0,0 +1,4 @@
+/****** What is the total Number of Deaths  ******/
+SELECT MAX([Deaths]) AS Total_Number_of_Deaths
+FROM [NCHS - Leading Causes of Death: United States].[dbo].['NCHS_-_Leading_Causes_of_Death_$']
+  
