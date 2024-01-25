306fd95cf7be84a18be8c644b06ccced53c79d07
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-25 09:32:07 +0300

    final

--- Projects/webscraping - Javascript/IMBD Movie Website/index.js
+++ Projects/webscraping - Javascript/IMBD Movie Website/index.js
@@ -1,4 +1,10 @@
-const request = require('require-promise');
-const cheerio =  require('cheerio');
+const request = require('request-promise');
+const cheerio = require('cheerio');
 
+const URL = 'https://www.imdb.com/title/tt0102926/?ref_=tt_mv_close';
 
+(async () => {
+    const response = await request(URL);
+
+    console.log(response);
+})();
