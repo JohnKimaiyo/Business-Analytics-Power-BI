2a40ef01f7279a200629ea58d0c50ac4188f4106
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-01-25 10:18:54 +0300

    final

--- Projects/webscraping - Javascript/IMBD Movie Website/index.js
+++ Projects/webscraping - Javascript/IMBD Movie Website/index.js
@@ -11,8 +11,12 @@ const URL = 'https://www.imdb.com/title/tt0102926/?ref_=tt_mv_close';
         // Find the title element and extract its text
         let title = $('span.hero__primary-text').text().trim();
 
+        let rating = $('div[data-testid="hero-rating-bar__aggregate-rating__score"] > span').text().trim();
+
         console.log(title);
+        console.log(rating)
     } catch (error) {
         console.error('Error:', error);
     }
 })();
+
